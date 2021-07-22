267bb0895dc64c7c0fd8cce8c636d2ea3e302f37
Timur Baburin <timurbabs@gmail.com>
2021-07-22 19:38:42 +0300

    Fixed environment variable shebang

--- roles/jenkins/templates/jenkins_groovy/initialization/security.groovy
+++ roles/jenkins/templates/jenkins_groovy/initialization/security.groovy
@@ -1,10 +1,9 @@
-#!groovy
+#!/usr/bin/env groovy
 
 import jenkins.model.*
 import hudson.security.*
 
 def instance = Jenkins.getInstance()
-
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 
 hudsonRealm.createAccount('{{ jenkins_username }}','{{ jenkins_password }}')
--- roles/jenkins/templates/jenkins_groovy/initialization/setup.groovy
+++ roles/jenkins/templates/jenkins_groovy/initialization/setup.groovy
@@ -1,4 +1,4 @@
-#!groovy
+#!/usr/bin/env groovy
 
 import jenkins.model.*
 import hudson.util.*;
