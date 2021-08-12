c78c2cea69a15e03ae373bc82f0de0c16c41e6a4
rozdestvenskiy <dimar08092@gmail.com>
2021-08-12 13:23:01 +0300

    main.tf added

--- net.tf
+++ net.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-    access_key = var.access_key
-    secret_key = var.secret_key
-    region = var.region
-}
-
 resource "aws_subnet" "PetclinicNet" {
     vpc_id     = "vpc-d1b531b8"
     cidr_block = "172.31.64.0/20"
