947ab99ec925035e11496720a3744ac944762dd4
rozdestvenskiy <dimar08092@gmail.com>
2021-08-18 12:41:03 +0300

    Code formatted

--- PetclinicTF/Production/main.tf
+++ PetclinicTF/Production/main.tf
@@ -1,12 +1,12 @@
 variable "StageTag" {
-    default = "Dev"
+  default = "Dev"
 }
 variable "NetCIDR" {
-    default = "172.31.80.0/20"
+  default = "172.31.80.0/20"
 }
 
 module "Infrustructure" {
-  source = "../modules/petclinic-infrustructure"
+  source   = "../modules/petclinic-infrustructure"
   StageTag = var.StageTag
-  NetCIDR = var.NetCIDR
+  NetCIDR  = var.NetCIDR
 }
--- PetclinicTF/Staging/main.tf
+++ PetclinicTF/Staging/main.tf
@@ -1,12 +1,12 @@
 variable "StageTag" {
-    default = "Stag"
+  default = "Stag"
 }
 variable "NetCIDR" {
-    default = "172.31.48.0/20"
+  default = "172.31.48.0/20"
 }
 
 module "Infrustructure" {
-  source = "../modules/petclinic-infrustructure"
+  source   = "../modules/petclinic-infrustructure"
   StageTag = var.StageTag
-  NetCIDR = var.NetCIDR
+  NetCIDR  = var.NetCIDR
 }
