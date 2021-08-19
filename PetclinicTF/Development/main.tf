65a26769b2e884260d3f0ad5aff5b1d0a054161e
rozdestvenskiy <dimar08092@gmail.com>
2021-08-19 11:08:41 +0300

    Name fixes

--- PetclinicTF/Development/main.tf
+++ PetclinicTF/Development/main.tf
@@ -2,11 +2,11 @@ variable "StageTag" {
   default = "Dev"
 ***REMOVED***
 variable "NetCIDR" {
-  default = "172.31.64.0/20"
+  default = "172.31.48.0/20"
 ***REMOVED***
 
-module "Infrustructure" {
-  source   = "../modules/petclinic-infrustructure"
+module "Infrastructure" {
+  source   = "../modules/petclinic-infrastructure"
   StageTag = var.StageTag
   NetCIDR  = var.NetCIDR
 ***REMOVED***
--- PetclinicTF/Production/main.tf
+++ PetclinicTF/Production/main.tf
@@ -1,12 +1,12 @@
 variable "StageTag" {
-  default = "Dev"
+  default = "Prod"
 ***REMOVED***
 variable "NetCIDR" {
   default = "172.31.80.0/20"
 ***REMOVED***
 
-module "Infrustructure" {
-  source   = "../modules/petclinic-infrustructure"
+module "Infrastructure" {
+  source   = "../modules/petclinic-infrastructure"
   StageTag = var.StageTag
   NetCIDR  = var.NetCIDR
 ***REMOVED***
