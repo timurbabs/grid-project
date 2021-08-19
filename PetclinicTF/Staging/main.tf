d6a8d52be25b029fd273c2b534c724f03774fe51
rozdestvenskiy <dimar08092@gmail.com>
2021-08-19 11:08:41 +0300

    Name fixes

--- PetclinicTF/Staging/main.tf
+++ PetclinicTF/Staging/main.tf
@@ -2,11 +2,11 @@ variable "StageTag" {
   default = "Stag"
 }
 variable "NetCIDR" {
-  default = "172.31.48.0/20"
+  default = "172.31.64.0/20"
 }
 
-module "Infrustructure" {
-  source   = "../modules/petclinic-infrustructure"
+module "Infrastructure" {
+  source   = "../modules/petclinic-infrastructure"
   StageTag = var.StageTag
   NetCIDR  = var.NetCIDR
 }
